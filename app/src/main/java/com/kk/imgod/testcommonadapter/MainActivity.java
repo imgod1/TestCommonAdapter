78b87b9ee90d461207e4acfa83cd79e647b0c618
imgod1 <970720947@qq.com>
2016-07-09 20:16:59 +0800

    添加了上拉加载更多

--- app/src/main/java/com/kk/imgod/testcommonadapter/MainActivity.java
+++ app/src/main/java/com/kk/imgod/testcommonadapter/MainActivity.java
@@ -1,22 +1,18 @@
 package com.kk.imgod.testcommonadapter;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.zhy.adapter.recyclerview.CommonAdapter;
-import com.zhy.adapter.recyclerview.MultiItemTypeAdapter;
 import com.zhy.adapter.recyclerview.base.ViewHolder;
 import com.zhy.adapter.recyclerview.wrapper.HeaderAndFooterWrapper;
+import com.zhy.adapter.recyclerview.wrapper.LoadMoreWrapper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerview;
     private List<String> list;
+    CustomCommonAdapter commonAdapter;
+    LoadMoreWrapper loadMoreWrapper;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            loadMore();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             list.add("Hello World:" + i);
         }
 
-        CustomCommonAdapter commonAdapter = new CustomCommonAdapter<String>(this, R.layout.item_text, list) {
+        commonAdapter = new CustomCommonAdapter<String>(this, R.layout.item_text, list) {
             @Override
             protected void convertView(ViewHolder holder, String s, int position) {
                 holder.setText(R.id.txt_title, "内容:" + s + "position:" + position);
@@ -51,6 +56,16 @@ public class MainActivity extends AppCompatActivity {
         HeaderAndFooterWrapper headerAndFooterWrapper = new HeaderAndFooterWrapper(commonAdapter);
         View view = LayoutInflater.from(this).inflate(R.layout.item_text, recyclerview, false);
         headerAndFooterWrapper.addHeaderView(view);
+
+        loadMoreWrapper = new LoadMoreWrapper(headerAndFooterWrapper);
+        loadMoreWrapper.setLoadMoreView(R.layout.load_more);
+        loadMoreWrapper.setOnLoadMoreListener(new LoadMoreWrapper.OnLoadMoreListener() {
+            @Override
+            public void onLoadMoreRequested() {
+                handler.sendEmptyMessageDelayed(0, 3000);
+            }
+        });
+
         commonAdapter.setCustomOnItemClickListener(new CustomCommonAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View view, RecyclerView.ViewHolder holder, Object o, int position) {
@@ -63,8 +78,21 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-        recyclerview.setAdapter(headerAndFooterWrapper);
+        recyclerview.setAdapter(loadMoreWrapper);
 
     }
 
+    private int position = 20;
+
+    private void loadMore() {
+        int end = position + 20;
+        for (; position < end; position++) {
+            list.add("Load More:" + position);
+        }
+//        commonAdapter.notifyDataSetChanged();//这种方式只能加载出来一个.不知道什么鬼
+//        loadMoreWrapper.notifyDataSetChanged();//记得最后要用设置给RecyclerView的那个adapter进行刷新
+        //推荐下方的方式
+        recyclerview.getAdapter().notifyDataSetChanged();
+
+    }
 }
